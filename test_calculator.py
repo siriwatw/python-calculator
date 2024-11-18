8528b5f7f11491787a932181ae50fa5a4ce743fd
siriwatw <siriwat2004@gmail.com>
2024-11-18 09:44:38 +0000

    add, sub fin

--- test_calculator.py
+++ test_calculator.py
@@ -8,8 +8,13 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        # เพิ่ม
+        self.assertEqual(self.calc.add(-2, 1), -1)
 
     # Add the following test methods to the TestCalculator class:
+    def test_substract(self):
+        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(-9, -12), 3)
 
 if __name__ == '__main__':
     unittest.main()
